0d88c12422f9f90d9a4fc90672e69b86297dd5f2
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2025-08-12 21:53:28 +0530

    Update Longest-Substring-Distinct-Chars.cpp

---
+++ Sliding-Window/Longest-Substring-Distinct-Chars.cpp
@@ -0,0 +1,23 @@
+//Keep growing the window
+//Shrink it if you find a duplicate character
+//Be careful while shrinking
+//You might accidentally expand
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        unordered_map<char, int> seen;
+        int l = 0, maxlen = 0, n = s.length();
+
+        for(int r= 0;r<n;r++)
+        {
+            if(seen.find(s[r]) != seen.end() && seen[s[r]] >= l)
+                l = seen[s[r]]+1;
+            seen[s[r]] = r;
+            maxlen = max(maxlen, r-l+1);  
+        }
+        return maxlen;
+        
+    }
+};
