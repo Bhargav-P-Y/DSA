984d53b8592648a91b43f934b8930d23fffaaa52
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2025-08-26 08:37:41 +0530

    Update Valid-Parentheses.cpp

---
+++ Stack/Valid-Parentheses.cpp
@@ -0,0 +1,30 @@
+//Use a map or a list if they are too many things
+//Combine conditions & check
+//See the map initialization
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> mp = { 
+            {'}', '{'},
+            {']', '['},
+            {')', '('}
+        };
+
+        for(char ch: s)
+        {
+            if(mp.count(ch))
+            {
+                if(!st.empty() && st.top() == mp[ch])
+                    st.pop();
+                else
+                    return false;
+            }
+            else
+                st.push(ch);  
+        }
+        return st.empty();
+        
+    }
+};
