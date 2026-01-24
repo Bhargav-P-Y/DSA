1b30a6375186d24c9683cf35af982356a3cb7bb7
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2026-01-24 21:17:26 +0530

    Refactor isInterleave function with space optimization

--- 1D-DP/Interleaving-String.cpp
+++ 1D-DP/Interleaving-String.cpp
@@ -1,70 +1,89 @@
-/*
-Start with the most simplest step. Empty Strings
-THen fill boundaries
+// With space optimization
+// THe interleaving by defn enforces the |n-m| <=1 constraint
 
-Compare possibilities
-Ensure you keep track of minimum of substrings
+// Without space optimization
 
-Finally chekc if it is <=1
-
-Are we holding onto things we don't need? Space optimize
-*/
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.length(), m = s2.length();
-
-        // Exit early if it can't make it
         if(s3.length() != n+m) return false;
-
-        // Space optimization step
-        if(n<m)
-            return isInterleave(s2, s1, s3);
-
-        int MAXI = n+m+1;
-        vector<pair<int, int>> prev(m+1, {MAXI, MAXI}), cur(m+1, {MAXI, MAXI});
+        
+        vector<bool> cur(m+1, false);
 
         // Base case
-        prev[0] = {0, 0};
+        cur[0] = true;
 
-        // Initialize first row
+        // Initialize the 1st row
         for(int j = 1;j<=m;j++)
         {
-            if(s2.substr(0, j) == s3.substr(0, j))
-                prev[j] = {0, 1};
+            if(s2[j-1] == s3[j-1] && cur[j-1])
+                cur[j] = true;
         }
 
         // Fill with recurrence defined
         for(int i = 1;i<=n;i++)
         {
-            // Reset cur with all MAXI
-            fill(cur.begin(), cur.end(), make_pair(MAXI, MAXI));
-
-            // Fill the first column
-            if(s1.substr(0, i) == s3.substr(0, i))
-                cur[0] = {1, 0};
+            // Boundary case
+            cur[0] = (s1[i-1] == s3[i-1]) && cur[0];
 
             for(int j = 1;j<=m;j++)
             {
-                // Were we right before?
-                // Imp bcz if we were wrong before it doesn't amtter what we do 
-                // next as it will be wrong
-                if(s1[i-1] == s3[i+j-1])
-                {
-                    // Two cases
-                    // Either take the char at s1 or from s2
-                    cur[j].first = min(cur[j].first, prev[j].first);
-                    cur[j].first = min(cur[j].first, prev[j].second + 1);
-                }
-                if(s2[j-1] == s3[i+j-1])
-                {
-                    cur[j].second = min(cur[j].second, cur[j-1].second);
-                    cur[j].second = min(cur[j].second, cur[j-1].first+1);
-                }
+                // Two cases
+                // Either take the char at s1 or from s2
+                bool cond1 = (s1[i-1] == s3[i+j-1]) && cur[j];
+                bool cond2 = (s2[j-1] == s3[i+j-1]) && cur[j-1];
+
+                cur[j] = cond1 || cond2;
+                
             }
-            prev = cur;
         }
-        return min(prev[m].first, prev[m].second) <= 1;
+        return cur[m];
+    }
+};
+
+
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.length(), m = s2.length();
+        if(s3.length() != n+m) return false;
+        
+        vector<bool> cur(m+1, false), nxt(m+1, false), init(m+1, false);
+
+        // Base case
+        cur[0] = true;
+
+        // Initialize the 1st row
+        for(int j = 1;j<=m;j++)
+        {
+            if(s2[j-1] == s3[j-1] && cur[j-1])
+                cur[j] = true;
+        }
+
+        // Fill with recurrence defined
+        for(int i = 1;i<=n;i++)
+        {
+            // Boundary case
+            if(s1[i-1] == s3[i-1] && cur[0])
+                nxt[0] = true;
+
+            for(int j = 1;j<=m;j++)
+            {
+                // Two cases
+                // Either take the char at s1 or from s2
+                if(s1[i-1] == s3[i+j-1])
+                    nxt[j] = nxt[j] || cur[j];
+
+                if(s2[j-1] == s3[i+j-1])
+                    nxt[j] = nxt[j] || nxt[j-1];
+
+            }
+            cur = nxt;
+            nxt = init;
+        }
+        return cur[m];
     }
 };
 
