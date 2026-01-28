c40cbc95818168c2f994ed1e3dce67b88f277c2a
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2026-01-28 13:27:23 +0530

    Add bottom-up DP solution for regex matching
    Implemented a bottom-up dynamic programming approach for regex matching.

--- 1D-DP/Regex-Matching.cpp
+++ 1D-DP/Regex-Matching.cpp
@@ -1,8 +1,49 @@
+// Bottom-up Approach
+class Solution {
+public:
+    bool isMatch(string s, string p) 
+    {
+        int n = s.length(), m = p.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, false));
+
+        /* 
+        Base case
+        Empty string & pattern is true
+        Index i represents the length
+        */
+        dp[0][0] = true;
+        
+        // First row
+        for(int j = 2;j<=m;j++)
+        {
+            if(p[j-1] == '*' && dp[0][j-2])
+                dp[0][j] = true;
+        }
+        for(int i = 1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+                if(p[j-1] == '.' || (s[i-1] == p[j-1]))
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')
+                {
+                    // Skip it
+                    dp[i][j] = dp[i][j-2];
+                    // See if it matches
+                    if(p[j-2] == '.' || p[j-2] == s[i-1])
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+
+
 /*
 Question your Base case!
 Consider both cases: Take & No take cases
 */
-
 class Solution {
 public:
     bool recur(string& s, string& p, int i, int j, 
