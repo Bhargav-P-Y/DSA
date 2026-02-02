99fb33565f41469ab52b3ee1a1c87bb903871d26
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2026-02-02 17:02:37 +0530

    Add solution for Hand of Straights problem
    Implement a solution for the Hand of Straights problem using an ordered map to manage frequency counts.

---
+++ Greedy/Hand-of-Straights.cpp
@@ -0,0 +1,30 @@
+/*
+Can't iterate and modify elemtns at the same time
+Use an ordered map for sorted order.
+*/
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        if(n % groupSize != 0) return false;
+
+        map<int, int> freq;
+        for(int num: hand)
+            freq[num]++;
+        
+        while(!freq.empty())
+        {
+            int start = freq.begin().first;
+            for(int i = 0;i<groupSize;i++)
+            {
+                int key = start + i;
+                if(freq.find(key) == freq.end()) return false;
+                freq[key] -=1;
+
+                if(freq[key] == 0) freq.erase(key);
+            }
+        }
+        return true;
+    }
+};
