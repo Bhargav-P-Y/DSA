f61a7000954449c1383d232ca0ec8bf8087e32c1
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2026-03-09 15:54:39 +0530

    Create Missing-Number.py

---
+++ Bit-Manipulation/Missing-Number.py
@@ -0,0 +1,14 @@
+'''
+XOR, identical things cancel out
+'''
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = 0
+
+        for i in range(len(nums)):
+            res ^= (i+1)
+            res ^= (nums[i])
+        
+        return res
+        
