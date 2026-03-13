9fd873b635cc16936ccea4c247c2b6d336ac8038
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2026-03-13 14:30:48 +0530

    Create Longest-Common-Prefix.py

---
+++ Two-Pointers/Longest-Common-Prefix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        '''
+        Assume 1st word is answer, trim it if necessary
+        Alternate approach, check vertically
+        The ith character for every word upto minlen of all words
+        '''
+
+        pref, n = strs[0], len(strs)
+
+        for word in strs:
+            minlen = min(len(pref), len(word))
+
+            for i in range(minlen):
+                if pref[i] != word[i]:
+                    pref = pref[:i]
+                    break
+        
+        return pref
