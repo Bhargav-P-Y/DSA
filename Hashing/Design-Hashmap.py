ef18d285427a01565fefa63701dd2a3e102db6e7
Bhargav-P-Y <161855573+Bhargav-P-Y@users.noreply.github.com>
2026-03-31 17:55:25 +0530

    Refactor BST methods and update MyHashMap calls

--- Hashing/Design-Hashmap.py
+++ Hashing/Design-Hashmap.py
@@ -19,19 +19,88 @@ class TreeNode:
 
 class BST:
     def __init__(self):
-        self.root = TreeNode()
+        self.root = None
     
     def insert(self, cur, key, val):
         # recursively check and insert into left or right subtree
-        pass
+
+        # Base case
+        if not cur:
+            return TreeNode(key, val)
+
+        # Already exists then update & return
+        if cur.node[0] == key:
+            cur.node[1] = val
+            return
+        
+        if cur.node[0] > key:
+            # recurse with left subtree
+            cur.left = self.insert(cur.left, key, val)
+        else:
+            # otherwise the right subtree
+            cur.right = self.insert(cur.right, key, val)
+
+
+    def minValueNode(self, cur):
+        while cur.left:
+            cur = cur.left
+        return cur
+
+    def delete(self, root, key):
+        '''
+        Three cases
+        Both children exist
+        Only left exists
+        Only right exists
+        Nothing exists
+        '''
+        # Base case
+        if not root:
+            return None
+        
+        if root.node[0] < key:
+            root.right = self.delete(root.right, key)
+        
+        elif root.node[0] > key:
+            root.left = self.delete(root.left, key)
+        
+        else:
+            # Left doesn't exist
+            # return right child
+            if not root.left:
+                return root.right
+            # Right doesn't exist
+            # return left child
+            elif not root.right:
+                return root.left
+            else:
+                # find inorder successor
+                tmp = self.minValueNode(root.right)
+                root.node = tmp.node
+
+                root.right = self.delete(root.right, tmp.node[0])
+                
+
+    def search(self, cur, key):
+        while cur:
+            if cur.node[0] == key:
+                return cur.node[1]
+            elif cur.node[0] > key:
+                cur = cur.left
+            else:
+                cur = cur.right
+
+        return -1
+
+
+    def add(self, key, value):
+        self.root = self.insert(self.root, key, value)
     
     def remove(self, key):
-        # recursively check and delete
-        pass
+        self.root = self.delete(self.root, key)
     
-    def search(self, key):
-        # search via iteration
-        pass
+    def contains(self, key):
+        return self.search(self.root, key)
 
 
 class MyHashMap:
@@ -45,13 +114,13 @@ class MyHashMap:
     def put(self, key: int, value: int) -> None:
         pos = self.findPos(key)
         cur = self.hm[pos]
-        cur.insert(key, value)
+        cur.add(key, value)
 
         
     def get(self, key: int) -> int:
         pos = self.findPos(key)
         cur = self.hm[pos]
-        return cur.search(key)
+        return cur.contains(key)
         
 
     def remove(self, key: int) -> None:
